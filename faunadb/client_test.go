fab9c8b46f0ff5bdd1fe2382323cf2946cc6b38f
Erick Pintor <erickpintor@gmail.com>
2016-08-10 09:26:58 -0300

    First scratch of FaunaClient
    * first scratch of FaunaClient
    * using base64 encoding for basic auth

---
+++ faunadb/client_test.go
@@ -0,0 +1,20 @@
+package faunadb
+
+import "testing"
+
+func TestEcho(t *testing.T) {
+	client := &FaunaClient{
+		Secret:   "secret",
+		Endpoint: "http://localhost:8443",
+	}
+
+	res, err := client.Query("HI")
+	if err != nil {
+		t.Error(err)
+	}
+
+	expected := "{\"resource\":\"HI\"}"
+	if res != expected {
+		t.Errorf("Expected: %s got: %s", expected, res)
+	}
+}
