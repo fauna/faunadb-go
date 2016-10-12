844d58ad125887bb2119ed29bf3ce3fa6a6895d0
Erick Pintor <erickpintor@gmail.com>
2016-10-12 16:05:17 -0300

    adding docs for expressions

--- faunadb/expr.go
+++ faunadb/expr.go
@@ -2,6 +2,7 @@ package faunadb
 
 import "encoding/json"
 
+// Expr represents query language expressions.
 type Expr interface {
 	expr() // Make sure only internal structures can be marked as valid expressions
 }
@@ -18,15 +19,22 @@ func (inv invalidExpr) MarshalJSON() ([]byte, error) {
 	return nil, inv.err
 }
 
+// Obj is a expression shortcut to represent any valid JSON object
 type Obj map[string]interface{}
+
+// Arr is a expression shortcut to represent any valid JSON array
 type Arr []interface{}
 
 func (obj Obj) expr() {}
 func (arr Arr) expr() {}
 
+// MarshalJSON implements json.Marshaler for Obj expression
 func (obj Obj) MarshalJSON() ([]byte, error) { return json.Marshal(wrap(obj)) }
+
+// MarshalJSON implements json.Marshaler for Arr expression
 func (arr Arr) MarshalJSON() ([]byte, error) { return json.Marshal(wrap(arr)) }
 
+// OptionalParameter describes optional parameters for query language functions
 type OptionalParameter func(unescapedObj)
 
 func applyOptionals(options []OptionalParameter, fn unescapedObj) Expr {
