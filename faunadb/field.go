9de87ffd528dde72f46efa7b3a4eed754e38ca6b
Erick Pintor <erickpintor@gmail.com>
2016-10-13 10:06:44 -0300

    adding docs for field extractors

--- faunadb/field.go
+++ faunadb/field.go
@@ -1,33 +1,28 @@
 package faunadb
 
-type Field struct {
-	path path
-}
+// Field is a field extractor for FaunaDB values.
+type Field struct{ path path }
 
+// FieldValue describes an extracted field value.
 type FieldValue interface {
-	GetValue() (Value, error)
-	Get(i interface{}) error
+	GetValue() (Value, error) // GetValue returns the extracted FaunaDB value.
+	Get(i interface{}) error  // Get decodes a FaunaDB value to a native Go type.
 }
 
-func ObjKey(keys ...string) Field {
-	return Field{pathFromKeys(keys...)}
-}
+// ObjKey creates a field extractor for a JSON object based on the keys informed.
+func ObjKey(keys ...string) Field { return Field{pathFromKeys(keys...)} }
 
-func ArrIndex(indexes ...int) Field {
-	return Field{pathFromIndexes(indexes...)}
-}
+// ArrIndex creates a field extractor for a JSON array based on the indexes informed.
+func ArrIndex(indexes ...int) Field { return Field{pathFromIndexes(indexes...)} }
 
-func (f Field) At(other Field) Field {
-	return Field{f.path.subPath(other.path)}
-}
+// At creates a new field extractor based on the sub field informed.
+func (f Field) At(other Field) Field { return Field{f.path.subPath(other.path)} }
 
-func (f Field) AtKey(keys ...string) Field {
-	return f.At(ObjKey(keys...))
-}
+// AtKey creates a new field extractor based on the sub key informed.
+func (f Field) AtKey(keys ...string) Field { return f.At(ObjKey(keys...)) }
 
-func (f Field) AtIndex(indexes ...int) Field {
-	return f.At(ArrIndex(indexes...))
-}
+// AtIndex creates a new field extractor based on the sub index informed.
+func (f Field) AtIndex(indexes ...int) Field { return f.At(ArrIndex(indexes...)) }
 
 func (f *Field) get(value Value) FieldValue {
 	value, err := f.path.get(value)
@@ -39,16 +34,12 @@ func (f *Field) get(value Value) FieldValue {
 	return validField{value}
 }
 
-type validField struct {
-	value Value
-}
+type validField struct{ value Value }
 
 func (v validField) GetValue() (Value, error) { return v.value, nil }
 func (v validField) Get(i interface{}) error  { return v.value.Get(i) }
 
-type invalidField struct {
-	err error
-}
+type invalidField struct{ err error }
 
 func (v invalidField) GetValue() (Value, error) { return nil, v.err }
 func (v invalidField) Get(i interface{}) error  { return v.err }
