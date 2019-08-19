cf3cab06427092d7ef1dcae02d1db4ff7dcb4a70
Markus <endzeit@gmail.com>
2019-08-19 11:34:05 +0200

    Refactor Range, make args non-optional and rename

--- faunadb/newspec.go
+++ faunadb/newspec.go
@@ -1,29 +1,8 @@
 package faunadb
 
-// LowerBound is a new optional parameter
-func LowerBound(ref interface{}) OptionalParameter {
-	return func(fn unescapedObj) {
-		fn["lowerbound"] = wrap(ref)
-	}
-}
-
-// UpperBound is a new optional parameter
-func UpperBound(ref interface{}) OptionalParameter {
-	return func(fn unescapedObj) {
-		fn["upperbound"] = wrap(ref)
-	}
-}
-
-// Range will provide the ability to limit a set based on lower and upper bounds of its natural order.
-//
-// Parameters:
-//  set SetRef - A set reference
-//
-// Optional parameters:
-//  Range(set, lowerBound, upperBound)
-//
-func Range(set interface{}, options ...OptionalParameter) Expr {
-	return fn1("range", set, options...)
+// Range provides the ability to limit a set based on lower and upper bounds of its natural order.
+func Range(set, from, to interface{}) Expr {
+	return fn3("range", set, "from", from, "to", to)
 }
 
 // Other range-like predicates to be added are RangeLT, RangeLTE, RangeGT, and RangeGTE,
