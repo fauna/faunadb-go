cc0ee518787a931f5fca1265ae53bd091ebe8c8b
Vadim Balaban <vadim.balaban@fauna.com>
2021-04-15 13:11:59 +0300

    FaunaDB -> Fauna in doc.go

--- faunadb/doc.go
+++ faunadb/doc.go
@@ -1,17 +1,17 @@
 /*
-Package faunadb implements the FaunaDB query language for Golang applications.
+Package faunadb implements the Fauna query language for Golang applications.
 
-FaunaClient is the main client structure, containing methods to communicate with a FaunaDB Cluster.
+FaunaClient is the main client structure, containing methods to communicate with a Fauna Cluster.
 This structure is designed to be reused, so avoid making copies of it.
 
-FaunaDB's query language is composed of expressions that implement the Expr interface.
+Fauna's query language is composed of expressions that implement the Expr interface.
 Expressions are created using the query language functions found in query.go.
 
-Responses returned by FaunaDB are wrapped into types that implement the Value interface. This interface provides
-methods for transversing and decoding FaunaDB values into native Go types.
+Responses returned by Fauna are wrapped into types that implement the Value interface. This interface provides
+methods for transversing and decoding Fauna values into native Go types.
 
 The driver allows for the user to encode custom data structures. You can create your own struct
-and encode it as a valid FaunaDB object.
+and encode it as a valid Fauna object.
 
 	type User struct {
 		Name string
@@ -29,6 +29,6 @@ If you wish to control the property names, you can tag them with the "fauna" tag
 
 	user := User{"John", 24} // Encodes as: {"displayName": "John", "age": 24}
 
-For more information about FaunaDB, check https://fauna.com/.
+For more information about Fauna, check https://fauna.com/.
 */
 package faunadb
