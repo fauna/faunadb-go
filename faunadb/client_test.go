1282964738ac59b9fe969cb571d1bc06b863cf6a
Stanislav Parkhomenko <stanislav.parkhomenko@gmail.com>
2021-10-27 17:56:03 +0300

    chore: updated version module

--- faunadb/client_test.go
+++ faunadb/client_test.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/stretchr/testify/suite"
 
-	f "github.com/fauna/faunadb-go/v4/faunadb"
+	f "github.com/fauna/faunadb-go/v5/faunadb"
 )
 
 func TestRunClientTests(t *testing.T) {
