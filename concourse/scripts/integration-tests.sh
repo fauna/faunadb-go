c04afbb143dda6d4a7c4c9269c7a1f7e45182d49
Adam Bollen <adam.bollen@fauna.com>
2023-02-15 09:01:56 -0800

    Add wait to bash script

--- concourse/scripts/integration-tests.sh
+++ concourse/scripts/integration-tests.sh
@@ -4,5 +4,7 @@ set -eou
 
 apk add --update make gcc musl-dev
 
+while ! $(curl --output /dev/null --silent --fail --max-time 1 http://faunadb:8443/ping); do sleep 1; done
+
 make test
 make coverage
