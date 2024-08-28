33c5f81755efebf9975a7de51438ad6e51a9c0d7
James Rodewig <james.rodewig@fauna.com>
2024-08-28 14:42:48 -0400

    [DOCS-3302] Update package description

--- faunadb/doc.go
+++ faunadb/doc.go
@@ -1,17 +1,24 @@
 /*
-Package faunadb implements the FaunaDB query language for Golang applications.
+Package faunadb is deprecated.
 
-FaunaClient is the main client structure, containing methods to communicate with a FaunaDB Cluster.
+Deprecated: Fauna is decommissioning FQL v4 on June 30, 2025. This driver is not
+compatible with FQL v10, the latest version. Fauna accounts created after August
+21, 2024 must use FQL v10. Ensure you migrate existing projects to the official
+v10 driver by the v4 EOL date: https://github.com/fauna/fauna-go.
+
+Package faunadb implements FQL v4 for Golang applications.
+
+FaunaClient is the main client structure, containing methods to communicate with a Fauna Cluster.
 This structure is designed to be reused, so avoid making copies of it.
 
-FaunaDB's query language is composed of expressions that implement the Expr interface.
+FQL is composed of expressions that implement the Expr interface.
 Expressions are created using the query language functions found in query.go.
 
-Responses returned by FaunaDB are wrapped into types that implement the Value interface. This interface provides
-methods for transversing and decoding FaunaDB values into native Go types.
+Responses returned by Fauna are wrapped into types that implement the Value interface. This interface provides
+methods for transversing and decoding Fauna values into native Go types.
 
 The driver allows for the user to encode custom data structures. You can create your own struct
-and encode it as a valid FaunaDB object.
+and encode it as a valid Fauna object.
 
 	type User struct {
 		Name string
@@ -29,6 +36,6 @@ If you wish to control the property names, you can tag them with the "fauna" tag
 
 	user := User{"John", 24} // Encodes as: {"displayName": "John", "age": 24}
 
-For more information about FaunaDB, check https://fauna.com/.
+For more information about Fauna, check https://fauna.com/.
 */
 package faunadb
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/fauna/faunadb-go/v4
+module github.com/fauna/faunadb-go/v4 // Deprecated: Use github.com/fauna/fauna-go for the latest version of Fauna.
 
 require (
 	github.com/stretchr/testify v1.6.1
